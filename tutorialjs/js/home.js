bb145993fd93d32b4ddf49eef366d09046bf2b5a
Luiz <enderecofacildelembrar@outlook.com>
2018-09-10 22:54:36 -0300

    conversão de closures e generators para async await

--- tutorialjs/js/home.js
+++ tutorialjs/js/home.js
@@ -1,177 +1,60 @@
 'use strict';
 
-/**
- * Entry point function
- */
-define(
-
-	[
-		'Index',
-		'JavaScriptLexer',
-		'HtmlLexer',
-		'Highlighter',
-		'HighlightEnhancer',
-		'NodeListIterator',
-		'domReadyPromise'
-
-	], (
-
-		Index,
-		JavaScriptLexer,
-		HtmlLexer,
-		Highlighter,
-		HighlightEnhancer,
-		NodeListIterator,
-		domReadyPromise
-
-	) => {
+import { domReadyPromise, NodeListIterator, Node } from '../../compartilhado/js/utils.js';
+import { Index } from '../../compartilhado/js/index.js';
+import { HighlightEnhancer } from '../../compartilhado/js/highlightEnhancer.js';
+import { JavaScriptLexer, HtmlLexer, Highlighter } from '../../compartilhado/js/highlighter.js';
 
 
-	/**
-	 * Firefox e Edge mostraram bugs em relação ao uso das novas funcionalidades
-	 * Generator e Promise quando utilizadas como aqui.
-	 * Usei closures para simular a funcionalidade e contornar tais bugs
-	 */
-	function highlightThemeClosure(blocosDeCodigo) {
 
-		let i = 0;
-		let blocoDeCodigo;
-		let iterador = blocosDeCodigo[Symbol.iterator]();
-		let iteradorTemp;
+async function highlightAsync(selector, Lexer) {
 
-		return function() {
+	try {
 
-			iteradorTemp = iterador.next();
-			if (iteradorTemp.done) {
-				return;
-			}
-			blocoDeCodigo = iteradorTemp.value;
-
-			blocoDeCodigo.className += ' bubaloop';
-			const highlightEnhancer = new HighlightEnhancer(blocoDeCodigo);
-
-			i++;
-			return blocoDeCodigo;
-
-		}
-
-	}
-
-
-	/**
-	 * Firefox e Edge mostraram bugs em relação ao uso das novas funcionalidades
-	 * Generator e Promise quando utilizadas como aqui.
-	 * Usei closures para simular a funcionalidade e contornar tais bugs
-	 */
-	function highlightClosure(blocosDeCodigo, Lexer) {
-
+		const blocosDeCodigo = new NodeListIterator(document.querySelectorAll(selector));
 		const lexer = new Lexer();
 		const highlighter = new Highlighter();
-		let blocoDeCodigo;
-		let iterador = blocosDeCodigo[Symbol.iterator]();
-		let iteradorTemp;
 
-		return function() {
-
-			iteradorTemp = iterador.next();
-			if (iteradorTemp.done) {
-				return;
-			}
-			blocoDeCodigo = iteradorTemp.value;
+		for (let blocoDeCodigo of blocosDeCodigo) {
 
 			const source = blocoDeCodigo.innerHTML;
-
 			blocoDeCodigo.className += ' bubaloop';
 
-			lexer.parseAsync(source)
-				.then((tokens) => {
-					return highlighter.highlightAsync(source, tokens);
-				})
-				.then((highlightedSource) => {
-					blocoDeCodigo.innerHTML = highlightedSource;
-					const highlightEnhancer = new HighlightEnhancer(blocoDeCodigo);
-				});
-
-			return blocoDeCodigo;
+			let tokens = await lexer.parseAsync(source);
+			let highlightedSource = await highlighter.highlightAsync(source, tokens);
+			blocoDeCodigo.innerHTML = highlightedSource;
+			const highlightEnhancer = new HighlightEnhancer(new Node(blocoDeCodigo));
 
 		}
 
+	} catch (erro) {
+		console.error('Erro ao iniciar a página. ' + erro + '\n' + erro.stack);
 	}
 
+}
 
-	function highlightWrapper(selector, Lexer, timeout) {
 
-		try {
 
-			if (timeout === null || timeout === undefined) {
-				timeout = 0;
-			}
+(async function() {
 
-			const blocosDeCodigo = new NodeListIterator(document.querySelectorAll(selector));
+	await domReadyPromise();
 
-			const iterator = highlightClosure(blocosDeCodigo, Lexer);
-			function callback() {
-				if (iterator()) {
-					setTimeout(callback, timeout);
-				}
-			}
-			setTimeout(callback, timeout);
+	try {
+		const index = new Index('indice', 3, false);
+	} catch (erro) {
+		console.error('Erro ao iniciar a página. ' + erro + '\n' + erro.stack);
+	}
 
-		} catch (erro) {
-			console.error('Erro ao iniciar a página. ' + erro + '\n' + erro.stack);
+	try {
+		const divsDeCodigo = new NodeListIterator(document.querySelectorAll('div.codeblock'));
+		for (let divCodigo of divsDeCodigo) {
+			divCodigo.className += ' bubaloop';
 		}
-
+	} catch (erro) {
+		console.error('Erro ao iniciar a página. ' + erro + '\n' + erro.stack);
 	}
 
+	await highlightAsync('code.javascript', JavaScriptLexer);
+	await highlightAsync('code.html', HtmlLexer);
 
-
-	domReadyPromise()
-
-		.then(() => {
-			try {
-				const index = new Index('indice', 3, false);
-			} catch (erro) {
-				console.error('Erro ao iniciar a página. ' + erro + '\n' + erro.stack);
-			}
-		})
-
-		.then(() => {
-			try {
-				const divsDeCodigo = new NodeListIterator(document.querySelectorAll('div.codeblock'));
-				for (let divCodigo of divsDeCodigo) {
-					divCodigo.className += ' bubaloop';
-				}
-			} catch (erro) {
-				console.error('Erro ao iniciar a página. ' + erro + '\n' + erro.stack);
-			}
-		})
-
-		.then(() => {
-			try {
-				const blocosDeCodigo = new NodeListIterator(document.querySelectorAll('code.generic'));
-
-				const iterator = highlightThemeClosure(blocosDeCodigo);
-				function callback() {
-					if (iterator()) {
-						setTimeout(callback, 0);
-					}
-				}
-				setTimeout(callback, 0);
-
-			} catch (erro) {
-				console.error('Erro ao iniciar a página. ' + erro + '\n' + erro.stack);
-			}
-		})
-
-		.then(() => {
-			highlightWrapper('code.html', HtmlLexer);
-		})
-		.then(() => {
-			highlightWrapper('code.javascript', JavaScriptLexer);
-		});
-
-
-
-	}
-
-);
+})();
